24750fb42c88dd7b3239929b3283c72d16c9c9f9
Yashi03902 <64241111+Yashi03902@users.noreply.github.com>
2024-03-04 15:31:07 +0530

    Add files via upload

---
+++ experiment/simulation/js/js1.js
@@ -0,0 +1,9 @@
+function b1(){
+    document.getElementById('b1btn').innerHTML=' = {(1, 1), (0, 1)}';
+}
+function b2(){
+    document.getElementById('b2btn').innerHTML=' = {(-1, 0, 0), (0, -1, 0), (0, 0, -1)}';
+}
+function e1e2(){
+    document.getElementById('e1e2btn').innerHTML='e<sub>1</sub> = (1, 1), e<sub>2</sub> = (0, 1) <br> T(e<sub>1</sub>) = (2, 0) = -2(-1, 0) + 0(0, -1) <br> T(e<sub>1</sub>) = ';
+}
